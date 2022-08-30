07ce3c4c852b264d633f441167010812baf1c141
Henrique Carderan <rike.c.12@gmail.com>
2022-08-30 10:58:26 -0300

    PG5-23 <pasta validor criada e aalterado schema no login>

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,11 @@
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
 import { useContext, useState } from "react";
 import { UserContext } from "../../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import { Form, Header, Main } from "./styles";
-import {AiFillEye, AiFillEyeInvisible} from 'react-icons/ai'
-
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { loginSchema } from "../../validators/LoginUser";
 
 interface FormProps {
   email: string;
@@ -14,32 +13,24 @@ interface FormProps {
   errors?: string;
 }
 
-const schema = yup.object({
-  email: yup
-  .string()
-  .email("Must be an email")
-  .required("E-mail is required"),
-  password: yup
-  .string()
-  .required("Password is required"),
-
-}).required();
-
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
 
-  const [showPassword, setShowPassword] = useState(false)
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
+  const { loginUser } = useContext(UserContext);
 
-  const {loginUser} = useContext(UserContext);
-
-  const { register, handleSubmit, formState: { errors } } = useForm<FormProps>({
-    resolver: yupResolver(schema)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormProps>({
+    resolver: yupResolver(loginSchema),
   });
 
   const handleBtnClick = () => {
-   setShowPassword(prevState => !prevState)
-  }
+    setShowPassword((prevState) => !prevState);
+  };
 
   return (
     <>
@@ -50,21 +41,36 @@ const Login = () => {
         <div className="form-container">
           <Form onSubmit={handleSubmit(loginUser)}>
             <label htmlFor="email">E-mail</label>
-            <input type="text" placeholder="E-mail" id="email" {...register("email")}/>
+            <input
+              type="text"
+              placeholder="E-mail"
+              id="email"
+              {...register("email")}
+            />
             <small>{errors.email?.message}</small>
             <label htmlFor="password">Password</label>
-            <input type={showPassword ? 'text' : 'password'} placeholder="Password" id="password" {...register("password")}/>
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              id="password"
+              {...register("password")}
+            />
             <small>{errors.password?.message}</small>
-            <button type="submit" onClick={() => navigate('/dashboard', { replace: true })}>Sign In</button>
+            <button
+              type="submit"
+              onClick={() => navigate("/dashboard", { replace: true })}
+            >
+              Sign In
+            </button>
             <button onClick={handleBtnClick} className="show-password">
-              {
-                showPassword ? <AiFillEyeInvisible size={'32'}/>
-                :               
-                <AiFillEye size={'32'}/>
-              }
-              </button>
+              {showPassword ? (
+                <AiFillEyeInvisible size={"32"} />
+              ) : (
+                <AiFillEye size={"32"} />
+              )}
+            </button>
           </Form>
-        </div>  
+        </div>
       </Main>
     </>
   );
