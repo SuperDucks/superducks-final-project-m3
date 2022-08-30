28bac0052ad1b0a98ca7438041e5d058a1e2bd5a
daragneri <daragneri@gmail.com>
2022-08-30 16:09:14 -0300

    feat: carrossel done

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,34 +1,106 @@
 import { Container, Header, Main } from "./styles";
-import logo from '../../assets/logo.png';
+import logo from "../../assets/logo.png";
 import { BtnFake, BtnPrimary } from "../../styles/buttons";
+import { SwiperSlide } from "swiper/react";
+import dead from './../../assets/dead.svg'
+import stranger from './../../assets/stranger.svg'
+import carro1 from './../../assets/img-carro-01.svg'
+import carro2 from './../../assets/carro2.svg'
+import carro3 from './../../assets/carro3.svg'
 
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
+// import required modules
+import { Navigation } from "swiper";
+import { Container2, Grid, MovieCardSlide, StyledSwiper } from "./styles";
 
 const Home = () => {
   return (
     <Container>
-    <Header>     
-        <figure>
-          <img src={logo} alt="logo"/>
-        </figure>
-        <div>
-          <BtnPrimary>Register</BtnPrimary>
-          <BtnFake>Sign UP</BtnFake>
+      <Header>       
+          <figure>
+            <img src={logo} alt="logo" />
+          </figure>
+          <div className="login-container">
+            <BtnPrimary>SIGN UP</BtnPrimary>
+            <BtnFake>LOG IN</BtnFake>
+          </div>       
+      </Header>
+      <Main>
+        <div className="title-box">
+          <h1>Agora ficou muito mais fácil encontrar filmes e séries</h1>
+          <h2>
+            Você não precisa mais gastar horas e horas procurando filmes em
+            catálogos extensos e intermináveis
+          </h2>
         </div>
-    </Header>
-    <Main>
-      <div className="title-box">
-        <h1>Agora ficou muito mais fácil encontrar filmes e séries</h1>
-        <h2>Você não precisa mais gastar horas e horas procurando filmes em catálogos extensos e intermináveis</h2>
-      </div>
         <section className="section-box">
-          <ul className="carrossel-box">          
-           <li>card1</li> {/* componente card */}
-          </ul>
+          <Grid>
+            <Container2>             
+              <div>               
+                <StyledSwiper                 
+                  slidesPerView={6}
+                  spaceBetween={20}
+                  grabCursor={true}
+                  slideVisibleClass={"swiper-visible"}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  navigation={true}
+                  modules={[Navigation]}
+                  className="mySwiper"
+                >
+                  <SwiperSlide>
+                    <MovieCardSlide>
+                      <img src={dead} alt="dead" />
+                    </MovieCardSlide>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <MovieCardSlide>
+                      <img src={stranger} alt="dead" />
+                    </MovieCardSlide>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <MovieCardSlide>
+                      <img src={carro1} alt="dead" />
+                    </MovieCardSlide>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <MovieCardSlide>
+                      <img src={carro2} alt="dead" />
+                    </MovieCardSlide>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <MovieCardSlide>
+                      <img src={carro3} alt="dead" />
+                    </MovieCardSlide>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <MovieCardSlide>
+                      <img src={carro1} alt="dead" />
+                    </MovieCardSlide>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <MovieCardSlide>
+                      <img src={dead} alt="dead" />
+                    </MovieCardSlide>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <MovieCardSlide>
+                      <img src={carro3} alt="dead" />
+                    </MovieCardSlide>
+                  </SwiperSlide>
+                </StyledSwiper>
+              </div>
+            </Container2>
+          </Grid>
         </section>
-    </Main>  
+      </Main>
     </Container>
-  )
-}
+  );
+};
 export default Home;
--- src/styles/buttons.ts
+++ src/styles/buttons.ts
@@ -3,17 +3,18 @@ import styled from "styled-components";
 export const BtnPrimary = styled.button`
   background: var(--color-primary);
   border-radius: 30px;
-  font: var(--font-title-8);
+  font: var(--font-title-9);
   font-weight: 600;
   color: var(--color-grey-4);
-  padding: 9px 39px;
-  
+  padding: 8px 28px;
 `;
 export const BtnFake = styled.button`
   background: transparent;
   font: var(--font-title-9);
   font-weight: 600;
   color: var(--color-grey-0);
+  padding: 6px 28px;
+
   
 `;
 export const BtnOutline = styled.button`
@@ -22,6 +23,6 @@ export const BtnOutline = styled.button`
   font: var(--font-title-9);
   font-weight: 600;
   color: var(--color-grey-4);
-  padding: 9px 39px;
+  
   
 `;
