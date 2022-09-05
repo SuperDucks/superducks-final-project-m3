24aaf8e2fe8f43d9880b39807ea1a1cccc1128d9
Henrique Carderan <rike.c.12@gmail.com>
2022-09-05 18:27:57 -0300

    PG5-49 <Cards made>

---
+++ src/components/CarouselSwiperMyList/index.tsx
@@ -0,0 +1,38 @@
+import { Slide, Slider, SliderProps } from "../Slider";
+
+import { IMoviesInfo } from "../../context/FilmContext/interfaces";
+import Card from "../Card";
+import { Container } from "./styles";
+
+const settings: SliderProps = {
+  spaceBetween: 5,
+  slidesPerView: 6,
+  navigation: true,
+  // pagination: {
+  //   clickable: true,
+  // },
+};
+
+function CarouselSwiperMyList({ moviesInfo }: IMoviesInfo) {
+  return (
+    <Container>
+      <h2>{moviesInfo.type}</h2>
+
+      <div>
+        <Slider settings={settings}>
+          {moviesInfo.movielist.map((movie) => {
+            return (
+              <>
+                <Slide key={movie.id}>
+                  <Card movie={movie}></Card>
+                </Slide>
+              </>
+            );
+          })}
+        </Slider>
+      </div>
+    </Container>
+  );
+}
+
+export default CarouselSwiperMyList;
